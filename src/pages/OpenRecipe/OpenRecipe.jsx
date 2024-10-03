bab463a74abf1581e693c53ebb5017872eb51084
Preethu Mary George <preethuedin@gmail.com>
2024-10-03 13:47:26 -0400

    create get function

--- src/pages/OpenRecipe/OpenRecipe.jsx
+++ src/pages/OpenRecipe/OpenRecipe.jsx
@@ -1,10 +1,36 @@
-
+import { useEffect, useState } from 'react'
 import "./OpenRecipe.scss";
+import axios from "axios";
+
 const OpenRecipe = () => {
+
+    const [recipes, setRecipes] = useState([])
+
+    const getRecipes = async () => {
+        const apiUrl = "http://localhost:8080/recipes"; 
+        try {
+            const response = await axios.get(apiUrl);
+            setRecipes(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error("Error fetching recipes:", error);
+        }
+    };
+
+      useEffect(() => {
+        getRecipes();
+      }, [])
 
     return (
         <>
-   
+            <h1>Recipes</h1>
+            <div className='recipes__list'>
+                {recipes.map((recipe) => (
+                <div key={recipe.id}>
+                <p>{recipe.name}</p>
+                </div>
+                ))}
+            </div>
         </>
     );
   };
