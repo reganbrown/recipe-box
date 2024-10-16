cdc2d9478cf5f209636c0d236b26c0953db04c29
reganbrown <regan_brown@icloud.com>
2024-10-16 14:41:22 -0300

    fix deployment issue

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,19 @@
-import HalfPage from "../../components/halfPage/halfPage";
+import HalfPage from "../../components/HalfPage/HalfPage";
 import { Link } from "react-router-dom";
 import "./HomePage.scss";
 const HomePage = () => {
-    return (
-        <>
-            <div className="container">
-                <Link className="links" to="/recipes">
-                    <HalfPage className="left" text="Open a Recipe" />
-                </Link>
-                <Link className="links" to="/CreateRecipe">
-                    <HalfPage className="right" text="Create a Recipe" />
-                </Link>
-            </div>
-        </>
-    );
-  };
-  
-  export default HomePage;
+  return (
+    <>
+      <div className="container">
+        <Link className="links" to="/recipes">
+          <HalfPage className="left" text="Open a Recipe" />
+        </Link>
+        <Link className="links" to="/CreateRecipe">
+          <HalfPage className="right" text="Create a Recipe" />
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
